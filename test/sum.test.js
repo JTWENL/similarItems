3228f2f49d938fe8a57b905f43c5232ee9ab9544
William <williameliason8@gmail.com>
2021-01-14 17:04:21 -0600

    added a test -> want to test circleCI

--- test/sum.test.js
+++ test/sum.test.js
@@ -6,7 +6,9 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('add5 to 3 to equal 8', () => {
   expect(sum.add5(3)).toBe(8);
-})
+});
 
-
+test('add5 to 4 to equal 9', () => {
+  expect(sum.add5(4)).toBe(9);
+});
 
