f97c268ba0aae09cb91595cfcf4c73af42a69fc2
William <williameliason8@gmail.com>
2021-01-29 23:09:25 -0600

    changing apiReq url

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 let similarItemsRequest = (col, value) => {
-  return axios.get(`http://localhost:8080/similar/${col}/${value}`)
+  return axios.get(`http://3.21.220.231:8080/similar/${col}/${value}`)
     .then(data => {return data.data})
     .catch(err => {
       if (err.response) {
